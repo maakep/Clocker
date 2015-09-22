c17c189d98e349aaa4b5aa361891763257e89c8b
Maakep <schon.fredrik@gmail.com>
2015-09-22 19:15:20 +0200

    True initial commit

---
+++ ClockerWebapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClockerWebapp.Startup))]
+namespace ClockerWebapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
