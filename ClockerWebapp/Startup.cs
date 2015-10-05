8dce93db3e16a61758ffadea0dc945b72a94bc0a
Haj <flummeltrollet@gmail.com>
2015-10-05 08:07:46 +0200

    Added realtime functions using signalR, currently only in about.cshtml. First update on branch, a lot more work needed til merge with master.

--- ClockerWebapp/Startup.cs
+++ ClockerWebapp/Startup.cs
@@ -8,6 +8,7 @@ namespace ClockerWebapp
     {
         public void Configuration(IAppBuilder app)
         {
+            app.MapSignalR();
             ConfigureAuth(app);
         }
     }
---
+++ ClockerWebapp/TimerCounter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SinalRtest3
+{
+    public class TimerCounter : Hub
+    {
+
+        private static int _connected=0; //To be changed to some database, sql/firebase
+        private static int _time = 0;
+        public TimerCounter()
+        {
+            //Connected();
+        }
+        
+        //Called after a connection is established.
+        public void Connected()
+        {
+            _connected++;
+            Clients.All.updateConnected(_connected);
+        }
+
+        //ONDISCONNECT doesn't work atm, but should update the value in the future.
+        //public override System.Threading.Tasks.Task OnDisconnected()
+        //{
+
+        //    this.Clients.All.updateConnected(--_connected);
+        //    return base.OnDisconnected()
+        //}
+
+        //Add timer stuff here later
+        public void Time()
+        {
+            _time++;
+            Clients.All.time(_time);
+        }
+    }
+}
